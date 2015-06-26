804947bdcdf6e5ea56bc7aa780faccb9910d0a08
黄勇 <yong.huang@jimubox.com>
2015-06-26 12:33:55 +0800

    添加新的分类

---
+++ ObjectiveSugar/NSDictionary+ObjectiveSugar.m
@@ -0,0 +1,72 @@
+//
+//  NSDictionary+ObjectiveSugar.m
+//  SampleProject
+//
+//  Created by Marin Usalj on 11/23/12.
+//  Copyright (c) 2012 @supermarin | supermar.in. All rights reserved.
+//
+
+#import "NSDictionary+ObjectiveSugar.h"
+
+@implementation NSDictionary (Rubyfy)
+
+- (void)each:(void (^)(id k, id v))block {
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        block(key, obj);
+    }];
+}
+
+- (void)eachKey:(void (^)(id k))block {
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        block(key);
+    }];
+}
+
+- (void)eachValue:(void (^)(id v))block {
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        block(obj);
+    }];
+}
+
+- (NSArray *)map:(id (^)(id key, id value))block {
+    NSMutableArray *array = [NSMutableArray array];
+
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        id object = block(key, obj);
+        if (object) {
+          [array addObject:object];
+        }
+    }];
+
+    return array;
+}
+
+- (BOOL)hasKey:(id)key {
+    return !!self[key];
+}
+
+- (NSDictionary *)pick:(NSArray *)keys {
+    NSMutableDictionary *picked = [[NSMutableDictionary alloc] initWithCapacity:keys.count];
+
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        if ([keys containsObject:key]) {
+            picked[key] = obj;
+        }
+    }];
+
+    return picked;
+}
+
+- (NSDictionary *)omit:(NSArray *)keys {
+    NSMutableDictionary *omitted = [[NSMutableDictionary alloc] initWithCapacity:([self allKeys].count - keys.count)];
+
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        if (![keys containsObject:key]) {
+            omitted[key] = obj;
+        }
+    }];
+
+    return omitted;
+}
+
+@end
--- README.md
+++ README.md
@@ -7,6 +7,11 @@
 4.	`UIAlertView+Extension`：增加UIAlertView的方法，例如使用block代替selector创建UIAlertView.
 5.	`UIImage+Gif`：一个使用Gif的UIImage的分类。
 6.	`NSDate+Escort`：一个方便使用NSDate的分类，添加了许多操作日期的方法，例如：dateAtEndOfDay返回某天的最后一个时刻的NSDate对象。
+7.	`NYXImagesKit`：<a href="https://github.com/Nyx0uf/NYXImagesKit" target="_blank">原作地址</a>增加UIImage的许多分类，包括设置图片大小，旋转图片，图片保存等。
+8.	`BlicksKit`：<a href="https://github.com/zwaldowski/BlocksKit" target="_blank">原作地址</a> 为Objective-C的许多类增加了分类，使用方式都改为block实现，让代码可读性更强！
+9.	`ObjectiveSugar`：<a href="https://github.com/supermarin/ObjectiveSugar" target="_blank">原作地址</a>对Foundation框架中的一些类，比如：NSArray，NSDictionary，NSString进行方法增强，使用的是ruby风格，非常好用。
+10.	`NSObject+AutoDescription`：<a href="https://github.com/djmadcat/NSObject-AutoDescription" target="_blank">原作地址</a>只要在description中调用一个方法，就会自动生成改类的描述。在LLDB的时候特别有用。
+
 
 ----------
 继续更新中...
